c1a7e70a9f0e3af0150e0abcab3cf4ee0483702d
iago rodrigues venâncio <166940571+Iagorodriguesvenancio@users.noreply.github.com>
2025-10-20 11:25:16 -0300

    Update enviar.js

--- assets/Javascript/enviar.js
+++ assets/Javascript/enviar.js
@@ -1,7 +1,12 @@
 async function enviar() {
+    if(document.querySelector('.inMensagem').value === '' || document.querySelector('.inEmail').value === '') {
+        alert('Por favor, preencha todos os campos antes de enviar sua mensagem.');
+        return;
+    }else {
     const resposta = await fetch('http://localhost:3000/teste', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ msm: document.querySelector('.inMensagem').value, email: document.querySelector('.inEmail').value })
     });
+    }
 }
